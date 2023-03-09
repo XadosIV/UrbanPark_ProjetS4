e24fd317288a86a2c66acb074631ccd4de2ee731
UP-4303 <up4303.pro@protonmail.com>
2023-03-09 23:46:01 +0100

    debug status

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 
 function App() {
-	return (
+	function GetData(){
+		fetch("http://localhost:3000/api/stuff")
+			.then((res) => {res.json(); console.log(res)})
+			.then(data => {
+				console.log(data);
+				return(
+					<div id="DATA">
+						DATA :
+						<ul>
+							{data ? data.map(item => {return <li>{item[0]}</li>}) : "no-data"}
+						</ul>
+					</div>
+				);
+			})
+			.catch(error => {
+				return(
+					<div id="ERROR">
+						ERROR :
+						<ul>
+							{error ? error.map(item => {return <li>{item[0]}</li>}) : "no-data"}
+						</ul>
+					</div>
+				);
+			})
+	}
+	return(
 		<div className="App">
-		
+			ALED
+			{GetData()}
 		</div>
 	);
 }
--- server/app.js
+++ server/app.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
 });
+
+app.use('/api/stuff', (req, res, next) => {
+	const stuff = [
+	  {
+		_id: 'oeihfzeoi',
+		title: 'Mon premier objet',
+		description: 'Les infos de mon premier objet',
+		imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+		price: 4900,
+		userId: 'qsomihvqios',
+	  },
+	  {
+		_id: 'oeihfzeomoihi',
+		title: 'Mon deuxième objet',
+		description: 'Les infos de mon deuxième objet',
+		imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+		price: 2900,
+		userId: 'qsomihvqios',
+	  },
+	];
+	res.status(200).json(stuff);
+  });
 
 module.exports = app;
