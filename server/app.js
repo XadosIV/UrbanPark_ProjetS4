c638c2707e78c7f29148928a802d78058b1cae2f
UP-4303 <up4303.pro@protonmail.com>
2023-03-09 18:14:40 +0100

    app.js avec express

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !!');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
