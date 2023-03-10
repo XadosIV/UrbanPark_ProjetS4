7d2598f7dbe44844b602980312435280546fc01e
UP-4303 <up4303.pro@protonmail.com>
2023-03-10 15:30:01 +0100

    abort merge

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// Default headers
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+});
+
+// Test endpoint
+app.use('/', (req, res, next) => {
+	let data = [];
+	res.status(200).json(data);
+});
+
+module.exports = app;
