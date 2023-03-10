9753ff30ab106570f59cdd5c11c47fe0ecba656f
UP-4303 <up4303.pro@protonmail.com>
2023-03-10 17:38:38 +0100

    Correctif de proposition

--- database.sql
+++ database.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE IF NOT EXISTS `UrbanPark`;
 
-CREATE TABLE IF NOT EXISTS `UrbanPark`.`Park_Lot` (
+CREATE TABLE IF NOT EXISTS `UrbanPark`.`Parking` (
 	id CHAR NOT NULL,
 	name VARCHAR(45) NOT NULL,
 	floors INT NOT NULL DEFAULT 1,
 	address VARCHAR(100) NOT NULL,
-	CONSTRAINT pk_park_lot PRIMARY KEY (id)
+	CONSTRAINT pk_parking PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS `UrbanPark`.`Parking_Spot` (
+CREATE TABLE IF NOT EXISTS `UrbanPark`.`Spot` (
 	id INT NOT NULL AUTO_INCREMENT,
 	number INT NOT NULL,
 	floor INT NOT NULL,
 	id_park CHAR NOT NULL,
-	CONSTRAINT pk_parking_spot PRIMARY KEY (id),
-	CONSTRAINT fk_parking_spot_park_lot FOREIGN KEY (id_park) REFERENCES `UrbanPark`.`Park_Lot` (id)
+	CONSTRAINT pk_spot PRIMARY KEY (id),
+	CONSTRAINT fk_spot_parking FOREIGN KEY (id_park) REFERENCES `UrbanPark`.`Parking` (id)
 );
 
 CREATE TABLE IF NOT EXISTS `UrbanPark`.`Role` (
@@ -29,22 +29,22 @@ CREATE TABLE IF NOT EXISTS `UrbanPark`.`User` (
 	email VARCHAR(50) NOT NULL,
 	role VARCHAR(45) NOT NULL,
 	-- role ENUM('ABONNE', 'GARDIEN', 'NETTOYAGE', 'ADMIN') NOT NULL DEFAULT 'Abonne',
-	id_parking_spot INT,
+	id_spot INT,
 	CONSTRAINT pk_user PRIMARY KEY (id),
 	CONSTRAINT fk_user_role FOREIGN KEY (role) REFERENCES `UrbanPark`.`Role` (name),
-	CONSTRAINT fk_user_parking_space FOREIGN KEY (id_parking_spot) REFERENCES `UrbanPark`.`Parking_Spot` (id),
+	CONSTRAINT fk_user_parking_space FOREIGN KEY (id_spot) REFERENCES `UrbanPark`.`Spot` (id),
 	CONSTRAINT uc_user UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS `UrbanPark`.`Schedule` (
 	id INT NOT NULL AUTO_INCREMENT,
 	id_user INT NOT NULL,
-	id_park_lot CHAR NOT NULL,
+	id_parking CHAR NOT NULL,
 	time_start DATETIME NOT NULL,
 	time_end DATETIME NOT NULL,
 	CONSTRAINT pk_schedule PRIMARY KEY (id),
 	CONSTRAINT fk_schedule_user FOREIGN KEY (id_user) REFERENCES `UrbanPark`.`User` (id),
-	CONSTRAINT fk_schedule_park_lot FOREIGN KEY (id_park_lot) REFERENCES `UrbanPark`.`Park_Lot` (id)
+	CONSTRAINT fk_schedule_parking FOREIGN KEY (id_parking) REFERENCES `UrbanPark`.`Parking` (id)
 );
 
 CREATE TABLE IF NOT EXISTS `UrbanPark`.`Reservation` (
@@ -62,9 +62,9 @@ CREATE TABLE IF NOT EXISTS `UrbanPark`.`Type` (
 );
 
 CREATE TABLE IF NOT EXISTS `UrbanPark`.`Typed` (
-	id_parking_spot INT NOT NULL,
+	id_spot INT NOT NULL,
 	name_type VARCHAR(45) NOT NULL,
-	CONSTRAINT pk_typed PRIMARY KEY (id_parking_spot, name_type),
-	CONSTRAINT fk_typed_parking_spot FOREIGN KEY (id_parking_spot) REFERENCES `UrbanPark`.`Parking_Spot` (id),
+	CONSTRAINT pk_typed PRIMARY KEY (id_spot, name_type),
+	CONSTRAINT fk_typed_spot FOREIGN KEY (id_spot) REFERENCES `UrbanPark`.`Spot` (id),
 	CONSTRAINT fk_typed_type FOREIGN KEY (name_type) REFERENCES `UrbanPark`.`Type` (name)
 );
