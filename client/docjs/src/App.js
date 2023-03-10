4032f834f7a05954e24af2518db2c729f1c930d8
Poutchy <enzo.mauti@laposte.net>
2023-03-10 19:26:52 +0100

    ajout du server react et son système de routage pour js et ts

---
+++ client/docjs/src/App.js
@@ -0,0 +1,65 @@
+import { AccessibilityNew } from "@mui/icons-material"
+import { Box, Stack, Typography,Button } from "@mui/material"
+import { AppRoutes } from "./AppRoutes"
+import { Link } from "react-router-dom";
+import React from "react";
+
+export function App() {
+	return (
+		<Box
+			sx={{ padding: 2 }}
+		>
+			<Stack
+				direction="row"
+				justifyContent="center"
+				alignItems="center"
+				spacing={2}
+
+				sx={{
+					marginBottom: 2,
+				}}
+			>
+			
+
+				<AccessibilityNew fontSize="large" />
+				<Link to="/">
+					<Typography variant="h3">home</Typography>
+				</Link>
+				<AccessibilityNew fontSize="large" />
+				<Link to="/test">
+					<Button variant="contained">go test</Button>
+			</Link>
+			</Stack>
+			<AppRoutes />
+		</Box>
+	)
+}
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ client/docjs/src/index.js
@@ -0,0 +1,35 @@
+import React,{StrictMode} from 'react';
+import './index.css';
+import {App} from "./App";
+import { BrowserRouter } from "react-router-dom"
+import { createRoot } from 'react-dom/client';
+
+
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+                    <App />
+        </BrowserRouter>
+    </StrictMode>
+)
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
