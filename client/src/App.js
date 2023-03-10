6ff39642d34649f7513543f088240ba2ebb4524c
UP-4303 <up4303.pro@protonmail.com>
2023-03-10 14:35:51 +0100

    front fonctionnel

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,49 @@
 import './App.css';
+import React from 'react';
 
 function App() {
-	function GetData(){
-		fetch("http://localhost:3000/api/stuff")
-			.then((res) => {res.json(); console.log(res)})
-			.then(data => {
-				console.log(data);
-				return(
-					<div id="DATA">
-						DATA :
-						<ul>
-							{data ? data.map(item => {return <li>{item[0]}</li>}) : "no-data"}
-						</ul>
-					</div>
-				);
-			})
-			.catch(error => {
-				return(
-					<div id="ERROR">
-						ERROR :
-						<ul>
-							{error ? error.map(item => {return <li>{item[0]}</li>}) : "no-data"}
-						</ul>
-					</div>
-				);
-			})
-	}
-	return(
-		<div className="App">
-			ALED
-			{GetData()}
-		</div>
-	);
-}
+	const [error, setError] = React.useState(null);
+	const [isLoaded, setIsLoaded] = React.useState(false);
+	const [items, setItems] = React.useState([]);
 	
+	// Remarque : le tableau vide de dépendances [] indique
+	// que useEffect ne s’exécutera qu’une fois, un peu comme
+	// componentDidMount()
+	React.useEffect(() => {
+		fetch("http://localhost:3001/api/stuff")
+		.then(res => res.json())
+		.then(
+			(result) => {
+				setIsLoaded(true);
+				setItems(result);
+			},
+			// Remarque : il faut gérer les erreurs ici plutôt que dans
+			// un bloc catch() afin que nous n’avalions pas les exceptions
+			// dues à de véritables bugs dans les composants.
+			(error) => {
+				setIsLoaded(true);
+				setError(error);
+			}
+		)
+	}, [])
+		
+	if (error) {
+		return <div>Erreur : {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Chargement...</div>;
+	} else {
+		return (
+			<ul>
+			{items.map(item => (
+				<li key={item._id}><ul>
+					{Object.keys(item).map(key => (
+						<li key={key}>{item[key]}</li>
+					))}
+				</ul></li>
+			))}
+			</ul>
+		);
+	}
+}
+
 export default App;
