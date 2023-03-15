7cd9979ee04642f34d2144ab7fa7a3409e678008
Poutchy <enzo.mauti@laposte.net>
2023-03-15 12:23:10 +0100

    exportation du système de vue pour être passer en test

---
+++ client/src/page/Connexion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ExampleName } from "../components";
+
+
+export function Connexion() {
+	return(<div>
+		<h1>Test Page</h1>
+		<p>Test page body </p>
+		<ExampleName name="John" age={20} />
+	</div>)
+}
