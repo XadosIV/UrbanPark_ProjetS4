c2ce7c99377634d3aa1beb6aed6957f5feb2d07a
UP-4303 <up4303.pro@protonmail.com>
2023-03-28 16:38:55 +0200

    testing database connection

--- server/app.js
+++ server/app.js
@@ -1,4 +1,14 @@
 const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql');
+const database = mysql.createConnection({
+	host     : process.env.HOST,
+	user     : process.env.USER,
+	password : process.env.PASSWORD,
+	database : process.env.DATABASE
+});
+
+database.connect();
 
 const app = express();
 
@@ -16,4 +26,4 @@ app.use('/', (req, res, next) => {
 	res.status(200).json(data);
 });
 
-module.exports = app;
+module.exports = {app, mysql, database};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./app');
+const {app, mysql, database} = require('./app');
 
 /**
  * NormalizePort  
