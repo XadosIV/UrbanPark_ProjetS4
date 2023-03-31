d8d6ae41eb11fb5bbdf2d8647b4836261d1d429f
UP-4303 <up4303.pro@protonmail.com>
2023-03-31 09:34:37 +0200

    typo query sql formatées

--- server/db_access/user.js
+++ server/db_access/user.js
@@ -8,7 +8,11 @@ require('dotenv').config();
  * @returns Array
  */
 function GetUsers(callback){
-	dbConnection.query("SELECT * FROM `"+process.env.DATABASE+"`.`Users`", callback);
+	dbConnection.query(`SELECT * FROM ${process.env.DATABASE}.Users;`, callback);
+}
+
+function PostUser(firstName, lastName, email, password, callback){
+	dbConnection.query(`INSERT INTO ${process.env.DATABASE}.Users first_name, last_name, email, password, role, token, id_spot VALUES ();`);
 }
 
 module.exports = {GetUsers};
