7ec8d4a6f8972b0f9cdb52f94338156b2118def9
Poutchy <enzo.mauti@laposte.net>
2023-04-01 22:25:08 +0200

    ajout de styles

--- client/src/components/liste.jsx
+++ client/src/components/liste.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from "react";
 
 export function Liste(element, funct, list) {
 
-	return (<div style={{display:"flex", flexWrap:"wrap"}}>
-		<ul>
+	return (<div style={{display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
+		<ul style={{minWidth:"60%", maxWidth:"100%"}}>
 			{
 				list.map((info, index) => (
-					<li onClick={funct} style={{backgroundColor:"red", flex:"800px"}}>
+					<li onClick={funct} style={{backgroundColor:"red", listStyle:"none"}}>
 						{element(info)}
 					</li>
 				))
 			}
 		</ul>
-		<div id="container" style={{backgroundColor:"blue"}}></div>
+		<div id="container" style={{backgroundColor:"blue", maxWidth:"30%", transitionDuration:"0.5s"}}></div>
 	</div>)
 }
