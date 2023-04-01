ac87862752aaeb98f7307000507461986013010a
MagicsssS <100727916+MagicsssS@users.noreply.github.com>
2023-04-01 22:40:44 +0200

    Majuscules aux noms des fichiers c'est tout

--- client/src/components/listePersonnes.jsx
+++ client/src/components/listePersonnes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ReactDOM from 'react-dom';
-import { Liste, Personne } from "./";
+import { Liste, Personne } from ".";
 import { creaListPerson } from "../interface"
 
 export function ListePersonnes(nom) {
