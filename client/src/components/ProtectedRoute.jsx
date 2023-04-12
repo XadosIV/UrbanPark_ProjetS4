c253baacf3606a12e7000cd1dc36018c3e69288c
Osteriux <pixave73@gmail.com>
2023-04-13 01:21:15 +0200

    typo

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-export function ProtectedRoutes ({ isAllowed, to }) {
-    return isAllowed ? <Outlet /> : <Navigate to={ to } /> ;
-}
