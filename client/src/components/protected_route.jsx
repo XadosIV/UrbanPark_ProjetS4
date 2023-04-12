1fbc65158b61199cad63c380e3c317cd7af2c7c6
Osteriux <pixave73@gmail.com>
2023-04-13 01:24:45 +0200

    make sense

--- client/src/components/protected_route.jsx
+++ client/src/components/protected_route.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 export function ProtectedRoutes ({ isAllowed, to }) {
-    return isAllowed ? <Outlet /> : <Navigate to={ to } /> ;
+    return isAllowed ? <Navigate to={ to } /> : <Outlet /> ;
 }
