939b14e865b48433e75af523f3bbcfd5b8cbc118
Poutchy <enzo.mauti@laposte.net>
2023-04-14 12:21:39 +0200

    passage du calendrier en français

--- client/src/components/view_agenda.jsx
+++ client/src/components/view_agenda.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar"
-import moment from "moment"
+import moment from "moment";
+import "moment/locale/fr"
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import events from "../data/events";
 
 export function AgendaTest (){
 	const localizer = momentLocalizer(moment);
+
+	const messages = {
+		allDays: "Tous les jours",
+		previous: "Précédent",
+		next: "Suivant",
+		today: "Aujourd'hui",
+		month: "Mois",
+		week: "Semaine",
+		day: "Jour",
+		agenda: "Agenda",
+		date:"Date",
+		time: "Heure",
+		event: "Evenement",
+	};
 
 	return (
 		<div>
@@ -15,6 +30,8 @@ export function AgendaTest (){
 				startAccessor="start"
 				endAccessor="end"
 				style={{height:500}}
+				culture="fr"
+				messages={messages}
 			/>
 		</div>
 	)
