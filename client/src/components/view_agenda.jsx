3e1a81bd72c5bcbeef053cc3a189fe5170175de0
Poutchy <enzo.mauti@laposte.net>
2023-04-18 20:25:04 +0200

    début de réaménagement de la vue

--- client/src/components/view_agenda.jsx
+++ client/src/components/view_agenda.jsx
@@ -3,34 +3,40 @@ import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment";
 import "moment/locale/fr"
 import "react-big-calendar/lib/css/react-big-calendar.css"
-import events from "../data/events";
 import { TakeAllEvents } from "../services";
+import take_parking from "../services/take_parking";
 
 export function AgendaTest (){
 	const localizer = momentLocalizer(moment);
 
 	const [eventsList, setEventsList] = useState([]);
+	const [parkingsList, setParkingsList] = useState([]);
 
 	function FormatSchedule (list)
 	{
-		let res = []
+		let sortie = []
 		let i = 0;
 		list.forEach(element => {
-			let parking = element.parking;
+			let idparking = element.parking;
 			let user = element.user;
 			let dateStart = element.date_start;
 			let dateEnd = element.date_end;
 
+			take_parking.TakeParking(idparking).then(res=> {
+
+			// console.log(res)
+				
 			let newElement = {
 				id: i,
-				title: "nettoyage parking " + String(parking) + " by " + String(user),
+				title: "nettoyage parking " + String(res[0].name) + " by " + String(user),
 				start: new Date(dateStart),
 				end: new Date(dateEnd),
 			  };
-			res.push(newElement);
+			sortie.push(newElement);
+			});
 		});
-		console.log(res);
-		return res;
+		setEventsList(sortie);
+		return sortie;
 	}
 
 	useEffect(() => {
