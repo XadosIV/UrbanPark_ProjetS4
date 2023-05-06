2123cf8a63c50ee4195fd611009a558f134e0fad
Poutchy <enzo.mauti@laposte.net>
2023-05-06 22:19:35 +0200

    maj du component spot et update_spot

--- client/src/components/update_spot.jsx
+++ client/src/components/update_spot.jsx
@@ -3,17 +3,55 @@ import Select from "react-select";
 import { Button } from "@mui/material";
 
 export function UpdateSpot (props) {
+	const base = props.used;
 	const [ typeList, setTypeList ] = useState(props.used);
 	const allTypes = props.allTypes;
-	console.log(allTypes)
+	
+	function AllTypes(list) {
+        var opt = []
+        for (let i=0; i<list.length; i++) {
+            opt.push({value:list[i].name, label:list[i].name})
+        }
+        return opt
+    }
 
 	const handleChangeSelect = (event) => {
-        var value = event.value;
-        setTypeList(values => ({...values, value}))
+		let modif = []
+		for (let type of event) {
+			modif.push({name:type.value})
+		}
+		setTypeList(values => (modif));
     }
 
 	const handleSubmit = (event) => {
+		console.log("b")
+		console.log(base)
+		console.log("l")
 		console.log(typeList)
+		let changes = [];
+		for (let typeL of typeList){
+			let trouve = false;
+			for (let typeB of base){
+				if (typeL.name == typeB.name){
+					trouve = true;
+				}
+			}
+			if (!trouve){
+				changes.push(typeL.name);
+			}
+		}
+		for (let typeB of base){
+			let trouve = false;
+			for (let typeL of typeList){
+				if (typeL.name == typeB.name){
+					trouve = true;
+				}
+			}
+			if (!trouve){
+				changes.push(typeB.name);
+			}
+		}
+		console.log(changes);
 	}
 
 	return (
@@ -22,12 +60,14 @@ export function UpdateSpot (props) {
 				isMulti
 				name="types"
 				placeholder="Choisir des types"
-				options={allTypes}
+				options={AllTypes(allTypes)}
 				className="search-add-two "
+				defaultValue={AllTypes(base)}
 				onChange={event => handleChangeSelect(event)}
-				defaultValue={[typeList]}
 			/>
-			<Button onClick={event => handleSubmit(event)}/>
+			<Button variant="contained" color="primary" onClick={handleSubmit}>
+				Modifier les types
+			</Button>
 		</form>
 	)
 }
