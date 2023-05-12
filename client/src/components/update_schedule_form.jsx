0c6c5e62e46c002e69380e9e7aa1104c863200e3
Poutchy <enzo.mauti@laposte.net>
2023-05-12 13:06:57 +0200

    ajout de la vue des évènements

--- client/src/components/update_schedule_form.jsx
+++ client/src/components/update_schedule_form.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import { UpdateSchedule, DeleteSchedule, CreationSchedule, TakeAllSpots, TakeParking, TakeByRole } from "../services"
-import { AllSpots, BaseSpot } from "../interface"
+import { AllSpots, BaseSpot, BaseParking } from "../interface"
 import Select from 'react-select';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css'
@@ -10,276 +10,317 @@ import ReactModal from 'react-modal';
 
 export function UpdateScheduleForm(props) {
 
-    /**
-     * DeOrDu
-     * Returns a string which is de or du depending of the type of the schedule
-     *
-     * @param { String } type - The type of schedule
-     * @return { Array }
-     */
-    function DeOrDu(type) {
-        if(type == "Réunion") {
-            return "de"
-        } else {
-            return "du"
-        }
-    }
+	/**
+	 * DeOrDu
+	 * Returns a string which is de or du depending of the type of the schedule
+	 *
+	 * @param { String } type - The type of schedule
+	 * @return { Array }
+	 */
+	function DeOrDu(type) {
+		if(type == "Réunion") {
+			return "de"
+		} else {
+			return "du"
+		}
+	}
 
-    /**
-     * AllParkings
-     * Returns a lists of options for a Select React component composed of every type 
-     *
-     * @param { Array } list - List of parkings
-     * @return { Array }
-     */
-    function AllParkings(list) {
-        var opt = []
-        for (let i=0; i<list.length; i++) {
-            opt.push({value:list[i].id, label:"Parking " + list[i].name.toLowerCase()})
-        }
-        return opt
-    }
+	/**
+	 * AllParkings
+	 * Returns a lists of options for a Select React component composed of every type 
+	 *
+	 * @param { Array } list - List of parkings
+	 * @return { Array }
+	 */
+	function AllParkings(list) {
+		var opt = []
+		for (let i=0; i<list.length; i++) {
+			opt.push({value:list[i].id, label:"Parking " + list[i].name.toLowerCase()})
+		}
+		return opt
+	}
 
-    /**
-     * AllServices
-     * Returns a lists of options for a Select React component composed of every type 
-     *
-     * @param { Array } list - List of service
-     * @return { Array }
-     */
-    function AllServices(list) {
-        var opt = []
-        for (let i=0; i<list.length; i++) {
-            opt.push({value:list[i].id, label:list[i].first_name + " " + list[i].last_name})
-        }
-        return opt
-    }
 
-    /**
-     * BaseParking
-     * Returns a string corresponding to the base parking
-     *
-     * @param { integer } id_park - id of the parking
-     * @param { Array } list - List of parkings
-     * @return { string }
-     */
-    function BaseParking(id_park, list) {
-        for (let parking of list) {
-            if (parking.id === id_park) {      
-                return "Parking " + parking.name.toLowerCase();
-            }
-        }
-    }
+	/**
+	 * BaseUser
+	 * Returns a array corresponding to the base user being passed in a react select defaultValue
+	 *
+	 * @param { integer } id_user - id of the user
+	 * @param { Array } list - List of users
+	 * @return { Array }
+	 */
+	function BaseUser(id_user, list) {
+		list = BaseListType(list)
+		var opts = []
+		if (!Array.isArray(id_user)) {
+			id_user = [id_user]
+		}
+		if (list) {
+			for (let user of list) {
+				for (let id of id_user) {
+					if (user.value === id) {
+						opts.push(user);
+					}
+				}
+			}
+			return opts
+		}
+	}
 
-    /**
-     * BaseListType
-     * Returns a array corresponding to the list of users corresponding to the type
-     *
-     * @param { string } spot - Type of the schedule
-     * @return { Array }
-     */
-    function BaseListType(type) {
-        if (type == "Gardiennage") {
-            return AllServices(guardiansList)
-        } else if (type == "Nettoyage") {
-            return AllServices(serviceList)
-        }
-    }
+	/**
+	 * BaseSpot
+	 * Returns a array corresponding to the base spot being passed in a react select defaultValue
+	 *
+	 * @param { integer } spot - id of the spot
+	 * @param { Array } list - List of options being passed in a react select
+	 * @return { Array }
+	 */
+	function BaseSpot(spot, list) {
+		var opts=[]
+		for (let s of list) {
+			if (s.value === spot) {
+				opts.push(s);
+			}
+		}
+		if (opts.length !== 0) {
+			return opts[0].label
+		} else {
+			return ""
+		}
+	}
 
-    /**
-     * BaseUser
-     * Returns a array corresponding to the base user being passed in a react select defaultValue
-     *
-     * @param { integer } id_user - id of the user
-     * @param { Array } list - List of users
-     * @return { Array }
-     */
-    function BaseUser(id_user, list) { 
-        list = BaseListType(list)
-        var opts = []
-        if (!Array.isArray(id_user)) {
-            id_user = [id_user]
-        }
-        if (list) {
-            for (let user of list) {
-                for (let id of id_user) {
-                    if (user.value === id) {
-                        opts.push(user);
-                    }       
-                }
-            }
-            return opts
-        }
-    }
+	/**
+	 * AllServices
+	 * Returns a lists of options for a Select React component composed of every type 
+	 *
+	 * @param { Array } list - List of service
+	 * @return { Array }
+	 */
+	function AllServices(list) {
+		if (!Array.isArray(list)) {
+			list = [list]
+		}
+		var opt = []
+		for (let i=0; i<list.length; i++) {
+			opt.push({value:list[i].id, label:list[i].first_name + " " + list[i].last_name})
+		}
+		return opt
+	}
 
-    /**
-     * BaseSpot
-     * Returns a array corresponding to the base spot being passed in a react select defaultValue
-     *
-     * @param { integer } spot - id of the spot
-     * @param { Array } list - List of options being passed in a react select
-     * @return { Array }
-     */
-    function BaseSpot(spot, list) {
-        var opts=[]
-        for (let s of list) {
-            if (s.value === spot) {
-                opts.push(s);
-            }
-        }
-        if (opts.length !== 0) {
-            return opts[0].label
-        } else {
-            return ""
-        }
-    }
+	/**
+	 * BaseListType
+	 * Returns a array corresponding to the list of users corresponding to the type
+	 *
+	 * @param { string } spot - Type of the schedule
+	 * @return { Array }
+	 */
+	function BaseListType(type) {
+		if (type == "Gardiennage") {
+			return AllServices(guardiansList);
+		} else if (type == "Nettoyage") {
+			return AllServices(serviceList);
+		} else if (type == "Réunion") {
+			return AllServices(serviceList).concat(AllServices(guardiansList));
+		}
+	}
 
-    const [optionsSpots, setOptionsSpots] = useState({opts:[], change:true})
+	function InformationEvent (infos, baseType) {
+		let baseList = props.baseUser;
+		if (!Array.isArray(baseList)) {
+			baseList = [baseList]
+		}
+		let listRes = Array()
+		for (let element of baseList) {
+			listRes.push(<li>{element.label}</li>)
+		}
+		let dates_start = props.event.start.toLocaleDateString() + " à " + props.event.start.toLocaleTimeString().slice(0, props.event.start.toLocaleTimeString().length-3)
+		let dates_end = props.event.end.toLocaleDateString() + " à " + props.event.end.toLocaleTimeString().slice(0, props.event.end.toLocaleTimeString().length-3)
+		return (
+			<div>
+				<ul>
+					{
+						listRes
+					}
+				</ul>
+				<p>
+					{
+						"Du " + dates_start + " au " + dates_end
+					}
+				</p>
+			</div>
+		);
+	}
 
-    
+	const [optionsSpots, setOptionsSpots] = useState({opts:[], change:true})
 
-    const [infos, setInfos] = useState({parking: props.event.idparking, 
-                                        user: props.event.user, 
-                                        date_start: props.event.d_st, 
-                                        date_end: props.event.d_en,
-                                        first_spot:props.event.first_spot,
-                                        last_spot:props.event.last_spot});
+
+	const [infos, setInfos] = useState(
+		{
+			parking: props.event.idparking,
+			user: props.event.user,
+			date_start: props.event.d_st,
+			date_end: props.event.d_en,
+			first_spot:props.event.first_spot,
+			last_spot:props.event.last_spot
+		}
+	);
 
 	const [wrongInput, setWrongInput] = useState(false);
-    const [errMessage, setErrMessage] = useState("");
+	const [errMessage, setErrMessage] = useState("");
 
-    const [parkingsList, setParkingsList] = useState([]);
-    const [serviceList, setServiceList] = useState([]);
-    const [guardiansList, setGuardiansList] = useState([]);
-    const [baseType, setBaseType] = useState(props.event.type)
-    
-    const [disabled, setDisabled] = useState(false)
-    const delay = ms => new Promise(res => setTimeout(res, ms));
+	const [parkingsList, setParkingsList] = useState([]);
+	const [serviceList, setServiceList] = useState([]);
+	const [guardiansList, setGuardiansList] = useState([]);
+	const [baseType, setBaseType] = useState(props.event.type)
 
-    const handleChangeSelect = (selectedOptions, name) => {
-        var value = [];
-        if (selectedOptions.value) {
-            if (name.name === "parking") {
-                TakeAllSpots(selectedOptions.value).then(res => {
-                    setOptionsSpots(values => ({...values, opts:AllSpots(res), change: true}))
-                })
-            }
-            value = selectedOptions.value
-        } else {
-            for (let option of selectedOptions) {
-                value.push(option.value)
-            }
-        }
-        setInfos(values => ({...values, [name.name]: value}))
-    }
+
+	const [disabled, setDisabled] = useState(false)
+	const delay = ms => new Promise(res => setTimeout(res, ms));
+
+	const handleChangeSelect = (selectedOptions, name) => {
+		var value = [];
+		if (selectedOptions.value) {
+			if (name.name === "parking") {
+				TakeAllSpots(selectedOptions.value).then(res => {
+					setOptionsSpots(values => ({...values, opts:AllSpots(res), change: true}))
+				})
+			}
+			value = selectedOptions.value
+		} else {
+			for (let option of selectedOptions) {
+				value.push(option.value)
+			}
+		}
+		setInfos(values => ({...values, [name.name]: value}))
+	}
 
 	const handlleSubmit = async (event) => {
-        event.preventDefault()
-        setWrongInput(false);
-        if (infos.user.length === 0) {
-            setWrongInput(true)
-            setErrMessage("Vous n'avez assigné ce créneau à personne")
-        }
-        else if (!(infos.parking === props.event.idparking && infos.user === props.event.user && infos.date_start === props.event.d_st && infos.date_end === props.event.d_en && infos.first_spot === props.event.first_spot && infos.last_spot === props.event.last_spot)) {
-            var scheduleAdded = 0;
-            var nbModif = 0;
-            let stock = infos.user
-            var fun;
-            for (let i=0; i<props.event.user.length; i++) {
-                infos.user = stock[i]
-                if (!(props.event.user.includes(infos.user))) {
-                    fun = DeleteSchedule(props.event.id_schedule[i]);
-                    nbModif++;
-                }
-                if (fun) {
-                    const res = await fun
-                    if (res.status === 200) {
-                        scheduleAdded++;
-                    } else {
-                        setWrongInput(true);
-                        setErrMessage(res.data.message);
-                        break;
-                    }
-                }
-            }
-            for (let i=0; i<stock.length; i++) {
-                infos.user = stock[i]
-                if (props.event.user.includes(infos.user)) {
-                    fun = UpdateSchedule(infos, props.event.id_schedule[i]);
-                    nbModif++;
-                } else {
-                    fun = CreationSchedule(infos);
-                    nbModif++;
-                }
-                if (fun) {
-                    const res = await fun
-                    if (res.status === 200) {
-                        scheduleAdded++;
-                    } else {
-                        setWrongInput(true);
-                        setErrMessage(res.data.message);
-                        break;
-                    }
-                }
-            }
-            if (scheduleAdded === nbModif) {
-                setWrongInput(true);
-                setErrMessage("Modification prise en compte.")
-                setDisabled(true)
-                await delay(2000);
-                props.handleCallback(false)
-            }
-            infos.user = stock
-        } else {
-            setWrongInput(true);
-            setErrMessage("Vous n'avez rien modifié");
-        }
-    }
+		event.preventDefault()
+		setWrongInput(false);
+		if (infos.user.length === 0) {
+			setWrongInput(true)
+			setErrMessage("Vous n'avez assigné ce créneau à personne")
+		}
+		else if (!(infos.parking === props.event.idparking && infos.user === props.event.user && infos.date_start === props.event.d_st && infos.date_end === props.event.d_en && infos.first_spot === props.event.first_spot && infos.last_spot === props.event.last_spot)) {
+			var scheduleAdded = 0;
+			var nbModif = 0;
+			let stock = infos.user
+			var fun;
+			for (let i=0; i<props.event.user.length; i++) {
+				infos.user = stock[i]
+				if (!(props.event.user.includes(infos.user))) {
+					fun = DeleteSchedule(props.event.id_schedule[i]);
+					nbModif++;
+				}
+				if (fun) {
+					const res = await fun
+					if (res.status === 200) {
+						scheduleAdded++;
+					} else {
+						setWrongInput(true);
+						setErrMessage(res.data.message);
+						break;
+					}
+				}
+			}
+			for (let i=0; i<stock.length; i++) {
+				infos.user = stock[i]
+				if (props.event.user.includes(infos.user)) {
+					fun = UpdateSchedule(infos, props.event.id_schedule[i]);
+					nbModif++;
+				} else {
+					fun = CreationSchedule(infos);
+					nbModif++;
+				}
+				if (fun) {
+					const res = await fun
+					if (res.status === 200) {
+						scheduleAdded++;
+					} else {
+						setWrongInput(true);
+						setErrMessage(res.data.message);
+						break;
+					}
+				}
+			}
+			if (scheduleAdded === nbModif) {
+				setWrongInput(true);
+				setErrMessage("Modification prise en compte.")
+				setDisabled(true)
+				await delay(2000);
+				props.handleCallback(false)
+			}
+			infos.user = stock
+		} else {
+			setWrongInput(true);
+			setErrMessage("Vous n'avez rien modifié");
+		}
+	}
 
-    useEffect(() => {
-        TakeParking().then(res => setParkingsList(res));
-        TakeAllSpots(infos.parking).then(res => {
-            setOptionsSpots({opts:AllSpots(res), change:false});
-        });
-        TakeByRole("Agent d'entretien").then(res => setServiceList(res));
-        TakeByRole("Gardien").then(res => setGuardiansList(res));
-    }, [])
+	useEffect(() => {
+		TakeParking().then(res => setParkingsList(res));
+		TakeAllSpots(infos.parking).then(res => {
+			setOptionsSpots({opts:AllSpots(res), change:false});
+		});
+		TakeByRole("Agent d'entretien").then(res => setServiceList(res));
+		TakeByRole("Gardien").then(res => setGuardiansList(res));
+	}, [props])
 
-    useEffect(() => {
-        TakeAllSpots(infos.parking).then(res => {
-            setOptionsSpots({opts:AllSpots(res), change:false})
-        })
-    }, [optionsSpots.change])
+	useEffect(() => {
+		setBaseType(props.event.type);
+		setInfos({
+			parking: props.event.idparking,
+			user: props.event.user,
+			date_start: props.event.d_st,
+			date_end: props.event.d_en,
+			first_spot: props.event.first_spot,
+			last_spot: props.event.last_spot
+		});
+	}, [props])
+
+
+	useEffect(() => {
+		TakeAllSpots(infos.parking).then(res => {
+			setOptionsSpots({opts:AllSpots(res), change:false})
+		})
+	}, [optionsSpots.change, props])
 
 	const customStyles = {
-        overlay: {
-            zIndex : 100000
-        },
-        content: {
-            top: '5%',
-            left: '25%',
-            right: '25%',
-            bottom: 'auto',
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection:"column",
-            marginRight: '-50%',
-            width: '50%'
-        },
-    };
+		overlay: {
+			zIndex : 100000
+		},
+		content: {
+			top: '5%',
+			left: '25%',
+			right: '25%',
+			bottom: 'auto',
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			flexDirection:"column",
+			marginRight: '-50%',
+			width: '50%'
+		},
+	};
 
-    return (
+	return (
 		<div>
 			<ReactModal
 				ariaHideApp={false}
-                isOpen={props.modalState}
-                contentLabel="Modifier le créneau"
-                onRequestClose={() => props.setModalState(false)}
-                style={customStyles}
+				isOpen={props.modalState}
+				contentLabel="Modifier le créneau"
+				onRequestClose={() => {
+					props.setModalState(false);
+				}}
+				style={customStyles}
 			>
 				<div className="info_reunion">
-					
+					<h3>{props.event.type}</h3>
+					<div>
+						{
+							InformationEvent(infos, baseType)
+						}
+					</div>
 				</div>
 				<div className="form_div">
 					<h3 style={{textAlign:"center"}}>Modification {DeOrDu(baseType)} {baseType.toLowerCase()} :</h3>
@@ -301,7 +342,7 @@ export function UpdateScheduleForm(props) {
 								isMulti
 								name="user"
 								options={BaseListType(baseType)}
-								defaultValue={BaseUser(infos.user, baseType)}
+								defaultValue={props.baseUser}
 								className="search-add-two"
 								onChange={handleChangeSelect}
 								maxMenuHeight={200}
@@ -363,6 +404,6 @@ export function UpdateScheduleForm(props) {
 				</div>
 			</ReactModal>
 		</div>
-    )
+	)
 }
 
--- client/src/components/view_agenda.jsx
+++ client/src/components/view_agenda.jsx
@@ -3,7 +3,7 @@ import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment";
 import "moment/locale/fr"
 import "react-big-calendar/lib/css/react-big-calendar.css"
-import { TakeAllEvents } from "../services";
+import { TakeAllEvents, TakeByRole } from "../services";
 import { UpdateScheduleForm } from "../components";
 
 export function ViewAgenda (props){
@@ -51,8 +51,12 @@ export function ViewAgenda (props){
 				if (MemeParking(idParking, sortie[j]) && MemeDepart(dateStart, sortie[j]) && MemeFin(dateEnd, sortie[j]))
 				{
 					trouve = true;
-					sortie[j].title += " and " + user;
-					sortie[j].user.push(element.user)
+					if (sortie[j].type !== "Réunion"){
+						sortie[j].title += " and " + user;
+					}
+					if (!(sortie[j].user.find(user => element.user === user))) {
+						sortie[j].user.push(element.user)
+					}
 					sortie[j].id_schedule.push(element.id)
 				}
 				j++;
@@ -64,7 +68,7 @@ export function ViewAgenda (props){
 					type : type,
 					id: i,
 					idparking: idParking,
-					title: type != "Reunion" ? type + " du parking " + parking + " par " + user : type,
+					title: type != "Réunion" ? type + " du parking " + parking + " par " + user : type,
 					start: new Date(dateStart),
 					d_st: dateStart,
 					d_en: dateEnd,
@@ -77,7 +81,6 @@ export function ViewAgenda (props){
 			}
 			i++;
 		});
-		console.log(sortie)
 		return sortie;
 	}
 
@@ -96,7 +99,6 @@ export function ViewAgenda (props){
 				role = props.props.role;
 			}
 		}
-		console.log(role)
 		TakeAllEvents(id, role).then(res => {
 			setEventsList(FormatSchedule(res));
 		})
@@ -124,6 +126,118 @@ export function ViewAgenda (props){
 		setUpdate(true)
 	}
 
+	const [serviceList, setServiceList] = useState([]);
+	const [guardiansList, setGuardiansList] = useState([]);
+
+	function AllServices(list) {
+		var opt = []
+		for (let i=0; i<list.length; i++) {
+			opt.push({value:list[i].id, label:list[i].first_name + " " + list[i].last_name})
+		}
+		return opt
+	}
+
+	/**
+	 * BaseParking
+	 * Returns a string corresponding to the base parking
+	 *
+	 * @param { integer } id_park - id of the parking
+	 * @param { Array } list - List of parkings
+	 * @return { string }
+	 */
+	function BaseParking(id_park, list) {
+		for (let parking of list) {
+			if (parking.id === id_park) {
+				return "Parking " + parking.name.toLowerCase();
+			}
+		}
+	}
+
+	/**
+	 * BaseListType
+	 * Returns a array corresponding to the list of users corresponding to the type
+	 *
+	 * @param { string } spot - Type of the schedule
+	 * @return { Array }
+	 */
+	function BaseListType(type) {
+		if (type == "Gardiennage") {
+			return AllServices(guardiansList);
+		} else if (type == "Nettoyage") {
+			return AllServices(serviceList);
+		} else if (type == "Réunion") {
+			return AllServices(serviceList).concat(AllServices(guardiansList));
+		}
+	}
+
+	/**
+	 * BaseUser
+	 * Returns a array corresponding to the base user being passed in a react select defaultValue
+	 *
+	 * @param { integer } id_user - id of the user
+	 * @param { Array } list - List of users
+	 * @return { Array }
+	 */
+	function BaseUser(id_user, list) {
+		list = BaseListType(list)
+		var opts = []
+		if (!Array.isArray(id_user)) {
+			id_user = [id_user]
+		}
+		if (list) {
+			for (let user of list) {
+				for (let id of id_user) {
+					if (user.value === id) {
+						opts.push(user);
+					}
+				}
+			}
+			return opts
+		}
+	}
+
+	/**
+	 * BaseListType
+	 * Returns a array corresponding to the list of users corresponding to the type
+	 *
+	 * @param { string } spot - Type of the schedule
+	 * @return { Array }
+	 */
+	function BaseListType(type) {
+		if (type == "Gardiennage") {
+			return AllServices(guardiansList);
+		} else if (type == "Nettoyage") {
+			return AllServices(serviceList);
+		} else if (type == "Réunion") {
+			return AllServices(serviceList).concat(AllServices(guardiansList));
+		}
+	}
+
+	/**
+	 * AllServices
+	 * Returns a lists of options for a Select React component composed of every type 
+	 *
+	 * @param { Array } list - List of service
+	 * @return { Array }
+	 */
+	function AllServices(list) {
+		if (!Array.isArray(list)) {
+			list = [list]
+		}
+		var opt = []
+		for (let i=0; i<list.length; i++) {
+			opt.push({value:list[i].id, label:list[i].first_name + " " + list[i].last_name})
+		}
+		return opt
+	}
+
+
+	useEffect(() => {
+		TakeByRole("Agent d'entretien").then(res => setServiceList(res));
+		TakeByRole("Gardien").then(res => setGuardiansList(res));
+	}, [props])
+
+
 	return (
 		<div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", width:"100%"}}>
 			<Calendar
@@ -137,7 +251,7 @@ export function ViewAgenda (props){
 				messages={messages}
 			/>
 			{selectedEvent && update &&
-				<UpdateScheduleForm event={selectedEvent} handleCallback={Callback} modalState={modalState} setModalState={setModalState}/>
+				<UpdateScheduleForm event={selectedEvent} handleCallback={Callback} modalState={modalState} setModalState={setModalState} baseUser={BaseUser(selectedEvent.user, selectedEvent.type)}/>
 		    }
 		</div>
 	)
