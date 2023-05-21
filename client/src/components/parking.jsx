3dea324c24dab9dc4fc84c22130eb5062846daa6
Osteriux <pixave73@gmail.com>
2023-05-21 22:08:51 +0200

    connexion carousel à l'api

--- client/src/components/parking.jsx
+++ client/src/components/parking.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TakeAllSpots } from "../services";
 
 /**
  * Composent of a Parking
  * @param { Parking } info
  * @return { Promise React.Component }
  */
+export function Parking({parking}) {
 
-export function Parking(parking) {
+	const [ allSpots, setAllspots ] = useState([]);
+	const [ nbFreeSpots, setNbFreeSpots ] = useState(0);
+
+	useEffect(() => {
+		async function fetchAllSpots(id_park){
+			let resAllSpots = await TakeAllSpots(id_park);
+			console.log("resAllSpots", resAllSpots);
+			setAllspots(resAllSpots);
+		}
+		fetchAllSpots(parking.id);
+	}, [parking])
+
+	useEffect(() => {
+		let lambdaSpots_notInCLeaning = allSpots.filter(spot => !spot.types.includes("Abonné") && !spot.in_cleaning);
+		setNbFreeSpots(lambdaSpots_notInCLeaning.length);
+	}, [allSpots])
 
 	return (
 		<div className="slides-item">
-			<h1>{parking.parking.nom}</h1>
-			<p>{parking.parking.nbPlaceLibre} / {parking.parking.nbPlaceTot}</p>
+			<h1>{ parking.name }</h1>
+			<h3>{ parking.address }</h3>
+			<p className="spot-counter" > { nbFreeSpots } / { allSpots.length }</p>
 		</div>)
 }
 
